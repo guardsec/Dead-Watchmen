b9745b08424e9bbb44f1cff177f968ea48df0456
TIBTHINK <bent23jam@gmail.com>
2019-11-21 21:27:20 -0500

    fuck you python and you snytax errors

--- src/menu/mainMenu.py
+++ src/menu/mainMenu.py
@@ -1,30 +1,30 @@
 from colorama import Fore, Style
 import colorama
+class Menu1:
+    def __init__ (self):
+        choice ='0'
+        while choice =='0':
+            print(Fore.WHITE + u"""
+            Main Choice: Choose 1 of 4 choices
+            1.begin
+            2.Check for updates
+            3.TrAiN)
+            4.exit""")
 
-def __init__ (self):
-    choice ='0'
-    while choice =='0':
-        print(Fore.WHITE + u"""
-        Main Choice: Choose 1 of 4 choices
-        1.begin
-        2.Check for updates
-        3.TrAiN)
-        4.exit""")
+            choice = input ("Please make a choice: ")
 
-        choice = input ("Please make a choice: ")
-
-        if choice == "4":
-            print()
-            exit(Fore.WHITE + u"thanks for stalking by")
-        elif choice == "3":
-            print("Do Something 3")
-        elif choice == "2":
-            print("Do Something 2")
-        elif choice == "1":
-            print("Do Something 1")
-        
-        else:
-            print("I don't understand your choice.")
+            if choice == "4":
+                print()
+                exit(Fore.WHITE + u"thanks for stalking by")
+            elif choice == "3":
+                print("Do Something 3")
+            elif choice == "2":
+                print("Do Something 2")
+            elif choice == "1":
+                print("Do Something 1")
+            
+            else:
+                print("I don't understand your choice.")
         
 
 
