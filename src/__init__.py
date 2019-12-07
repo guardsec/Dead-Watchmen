fd4103b9b79eabbb9f8b720d65ae0fdc5fde7d2c
tibthink <bent23jam@gmail.com>
2019-12-07 17:11:41 +0000

    adding in phishing module

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from .menu.boot import boot
-from .menu.menu1 import Mainmenu
+from .menu.menu import Mainmenu
 from .menu.logo import logo
 from .core.var.text import *
 from .core.whitepages import *
