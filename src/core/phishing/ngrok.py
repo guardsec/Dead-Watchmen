d183f071e10504a07196c12242630f6de471c7bd
tibthink <bent23jam@gmail.com>
2020-01-06 10:27:16 -0500

    dab on them haters

--- src/core/phishing/ngrok.py
+++ src/core/phishing/ngrok.py
@@ -19,8 +19,6 @@ def _run_ngrok():
     system = platform.system()
     if system == "Darwin":
         command = "ngrok"
-    elif system == "Windows":
-        command = "ngrok.exe"
     elif system == "Linux":
         command = "ngrok"
     else:
@@ -30,12 +28,12 @@ def _run_ngrok():
 
     ngrok = subprocess.Popen([executable, 'http', '5000'])
     atexit.register(ngrok.terminate)
-    localhost_url = "http://localhost:4040/api/tunnels"  # Url with tunnel details
+    localhost_url = "http://localhost:4040/api/tunnels"  # info about the next comment
     time.sleep(1)
-    tunnel_url = requests.get(localhost_url).text  # Get the tunnel information
+    tunnel_url = requests.get(localhost_url).text  # gets the cho cho train tunnle
     j = json.loads(tunnel_url)
 
-    tunnel_url = j['tunnels'][0]['public_url']  # Do the parsing of the get
+    tunnel_url = j['tunnels'][0]['public_url']  # Gets things
     tunnel_url = tunnel_url.replace("https", "http")
     return tunnel_url
 
@@ -46,8 +44,6 @@ def _download_ngrok(ngrok_path):
     system = platform.system()
     if system == "Darwin":
         url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
-    elif system == "Windows":
-        url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip"
     elif system == "Linux":
         url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
     else:
--- src/core/phishing/pyphisher.py
+++ src/core/phishing/pyphisher.py
@@ -1,3 +1,5 @@
+import ngrok
 from ..var.text import four0four
 def pyphisher():
-    four0four()
+#    four0four()
+	
