29ab710403b36745405298b5e0a52d1e10909493
tibthink <bent23jam@gmail.com>
2020-01-27 14:54:57 +0000

    dab

--- install.sh
+++ install.sh
@@ -27,10 +27,6 @@ echo "
 "
 sleep 2
 
-# if [[ $EUID -ne 0 ]]; then
-#    echo "This script must be run as root" 
-#    exit 1
-# fi
 
 if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
     INSTALL_DIR="$PREFIX/usr/share/doc/Dead-Watchmen"
@@ -41,6 +37,10 @@ if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
     pkg install -y sl git python3-pip python3 python-pip
     pip3 install -r requirements.txt
 else
+  if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+  fi
     INSTALL_DIR="$HOME/.Dead-Watchmen"
     BIN_DIR="/usr/local/bin/"
     BASH_PATH="/bin/bash"
